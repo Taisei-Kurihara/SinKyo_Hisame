82c12e81757d068d681a735f7eea3686bef88139
yuiTan <yuitanguen@gmail.com>
2026-03-09 16:03:45 +0900

    select画面からバトル画面まで推移するように修正

--- Game/Assets/Main/Script/InGame/Object/EventPointAbstract.cs
+++ Game/Assets/Main/Script/InGame/Object/EventPointAbstract.cs
@@ -2,6 +2,7 @@ using R3.Triggers;
 using UnityEngine;
 using R3;
 using InGame.Player;
+using Common;
 
 namespace GameCommon
 {
@@ -13,20 +14,81 @@ namespace GameCommon
         [SerializeField]
         private GameObject viewObject;
 
+        [Header("長押し発動設定")]
+        [SerializeField] private float holdMaxValue = 100f;
+        [SerializeField] private float holdSpeed = 50f;
+        [SerializeField] private bool resetOnRelease = true;
+
+        protected float holdValue = 0f;
+        protected bool isPlayerInside = false;
+        protected bool isEventInvoked = false;
+
+        private InputSystem_Actions inputActions;
+        private CompositeDisposable disposables = new CompositeDisposable();
+
         public void Awake()
         {
+            inputActions = InputSystemActionsManager.Instance().GetInputSystem_Actions();
+
             gameObject.OnTriggerEnter2DAsObservable()
-                .Where(_ => _?.GetComponent<PlayerAttach>() != null)
-                .Subscribe(_ => {
+                .Where(col => col != null && col.GetComponent<PlayerAttach>() != null)
+                .Subscribe(_ =>
+                {
                     InPlayer();
-                }).AddTo(gameObject);
+                })
+                .AddTo(disposables);
+
             gameObject.OnTriggerExit2DAsObservable()
-                .Where(_ => _?.GetComponent<PlayerAttach>() != null)
+                .Where(col => col != null && col.GetComponent<PlayerAttach>() != null)
                 .Subscribe(_ =>
                 {
                     OutPlayer();
-                }).AddTo(gameObject);
+                })
+                .AddTo(disposables);
+
+            Observable.EveryUpdate()
+                .Subscribe(_ =>
+                {
+                    UpdateHeartResistHold();
+                })
+                .AddTo(disposables);
         }
+
+        /// <summary>
+        /// HeartResist長押し進行処理
+        /// </summary>
+        private void UpdateHeartResistHold()
+        {
+            if (!isPlayerInside || isEventInvoked || inputActions == null)
+            {
+                return;
+            }
+
+            if (inputActions.CharacterController.HeartResist.IsPressed())
+            {
+                holdValue += holdSpeed * Time.deltaTime;
+                holdValue = Mathf.Clamp(holdValue, 0f, holdMaxValue);
+
+                // 必要ならここでUI更新
+                // OnHoldProgress(holdValue / holdMaxValue);
+
+                if (holdValue >= holdMaxValue)
+                {
+                    isEventInvoked = true;
+                    OnEvent();
+                }
+            }
+            else
+            {
+                if (resetOnRelease)
+                {
+                    holdValue = 0f;
+                    // 必要ならここでUI更新
+                    // OnHoldProgress(0f);
+                }
+            }
+        }
+
 
         public void PlayerSearch(){}
         /// <summary>
@@ -34,14 +96,28 @@ namespace GameCommon
         /// </summary>
         public void InPlayer()
         {
-            viewObject.SetActive(true);
+            isPlayerInside = true;
+            holdValue = 0f;
+            isEventInvoked = false;
+
+            if (viewObject != null)
+            {
+                viewObject.SetActive(true);
+            }
         }
         /// <summary>
         /// Playerが出たとき
         /// </summary>
         public void OutPlayer()
         {
-            viewObject.SetActive(false);
+            isPlayerInside = false;
+            holdValue = 0f;
+            isEventInvoked = false;
+
+            if (viewObject != null)
+            {
+                viewObject.SetActive(false);
+            }
         }
 
 
@@ -52,6 +128,11 @@ namespace GameCommon
         public virtual void OnEvent()
         {
             
+        }
+
+        public virtual void OnDestroy()
+        {
+            disposables?.Dispose();
         }
     }
 }
